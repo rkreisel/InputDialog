933e72ace3777ff13185ffd83ccd94f5e3a1c246
Randy Kreisel <randy@rkreisel.com>
2023-12-08 11:18:53 -0600

    Enabled use input in combobox and added parameter to use it conditionally

--- InputDialogUsageSample/Main.cs
+++ InputDialogUsageSample/Main.cs
@@ -8,7 +8,7 @@ public partial class Main : Form
     {
         InitializeComponent();
     }
-     
+
     private void btnSimpleTextInput_Click(object sender, EventArgs e)
     {
         var txt = cmLongText.Checked
@@ -57,19 +57,6 @@ public partial class Main : Form
             "Title",
             InputDialog.InputDialog.IDIcon.Question,
             InputDialog.InputDialog.IDButton.OkCancel);
-    }
-
-    private void btnComboBox_Click(object sender, EventArgs e)
-    {
-        var rslt = InputDialog.InputDialog.ShowDialog(
-            "This is the message.",
-            "Title",
-            InputDialog.InputDialog.IDIcon.Question,
-            InputDialog.InputDialog.IDButton.OkCancel,
-            type: InputDialog.InputDialog.IDType.ComboBox,
-            listItems: new List<string> { "Item 1", "Item2", "Item 3" });
-        if (rslt.DialogResult == DialogResult.OK)
-            ShowResult(rslt.ResultText);
     }
 
     private void btnComboBoxWithError_Click(object sender, EventArgs e)
@@ -159,5 +146,33 @@ public partial class Main : Form
     private static void ShowResult(string rslt)
     {
         MessageBox.Show(rslt, "InputDialog Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    private void btnEditableComboBox_Click(object sender, EventArgs e)
+    {
+        var rslt = InputDialog.InputDialog.ShowDialog(
+           "This is the message.",
+           "Title",
+           InputDialog.InputDialog.IDIcon.Question,
+           InputDialog.InputDialog.IDButton.OkCancel,
+           type: InputDialog.InputDialog.IDType.ComboBox,
+           listItems: new List<string> { "Item 1", "Item2", "Item 3" },
+           acceptsUserInput: true);
+        if (rslt.DialogResult == DialogResult.OK)
+            ShowResult(rslt.ResultText);
+    }
+
+    private void btnLockedComboBox_Click(object sender, EventArgs e)
+    {
+        var rslt = InputDialog.InputDialog.ShowDialog(
+            "This is the message.",
+            "Title",
+            InputDialog.InputDialog.IDIcon.Question,
+            InputDialog.InputDialog.IDButton.OkCancel,
+            type: InputDialog.InputDialog.IDType.ComboBox,
+            listItems: new List<string> { "Item 1", "Item2", "Item 3" },
+            acceptsUserInput: false);
+        if (rslt.DialogResult == DialogResult.OK)
+            ShowResult(rslt.ResultText);
     }
 }
